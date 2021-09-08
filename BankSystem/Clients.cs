2f5456d34436cece78f4cbe4759b240364388f65
Francisco Silvério <franciscogsilverio@gmail.com>
2021-09-08 09:36:53 -0300

    2 clients added as objects

---
+++ BankSystem/Clients.cs
@@ -0,0 +1,6 @@
+public class Clients{
+    public string owner;
+    public int agency;
+    public int account;
+    public double cash;
+}
---
+++ BankSystem/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BankSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clients Francisco = new Clients();
+            Francisco.owner = "Francisco";
+            Francisco.agency = 1234;
+            Francisco.account = 567;
+            Francisco.cash = 1000.50;
+
+            Clients Jose = new Clients();
+            Jose.owner = "Jose";
+            Jose.agency = 1234;
+            Jose.account = 675;
+            Jose.cash = 1250;
+
+            Console.WriteLine("The account owner is of Francisco is "+ Francisco.owner);
+            Console.WriteLine("The account owner is of Jose is "+ Jose.owner);
+
+        }
+    }
+}
